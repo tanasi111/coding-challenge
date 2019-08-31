a385eb5220e9214e9a40b999af3ff40562a21f48
tanasi111 <ivantanasi111@gmail.com>
2019-08-31 20:44:36 +0200

    dev

--- JavaScript/hackerrank/index.js
+++ JavaScript/hackerrank/index.js
@@ -1,6 +1,22 @@
 // Sock Merchant
 function sockMerchant(n, ar) {
 
+    const uniqueSocks = [...new Set(ar)];
+
+    let matchingPairs = 0;
+    uniqueSocks.forEach(function (uniqueSock) {
+        let counter = 0;
+        ar.forEach(function (sock) {
+            if (uniqueSock === sock) {
+                counter++;
+            }
+        });
+        matchingPairs += Math.floor(counter / 2);
+    });
+
+    console.log(matchingPairs);
 
 }
+
+sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20])
 
